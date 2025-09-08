95d0e6078654faed88fa70f0f4479a566d535104
Gabriel Habermann <gabrielfranh@gmail.com>
2025-09-08 00:18:51 -0300

    Create CQRS dispatchers, commands and events objects

---
+++ SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Dispatchers/CommandDispatcher.cs
@@ -0,0 +1,26 @@
+using CQRS.Core.Commands;
+using CQRS.Core.Infrastructure;
+
+namespace Post.Cmd.Infrastructure.Dispatchers
+{
+    public class CommandDispatcher : ICommandDispatcher
+    {
+        private readonly Dictionary<Type, Func<BaseCommand, Task>> _handlers = [];
+
+        public void RegisterHandler<T>(Func<T, Task> handler) where T : BaseCommand
+        {
+            if (_handlers.ContainsKey(typeof(T)))
+                throw new ArgumentException($"Handler for command type {typeof(T).Name} is already registered.");
+
+            _handlers.Add(typeof(T), x => handler((T)x));
+        }
+
+        public async Task SendAsync(BaseCommand command)
+        {
+            if (_handlers.TryGetValue(command.GetType(), out var handler))
+                await handler(command);
+            else
+                throw new ArgumentNullException(nameof(handler), "No command handler was registered.");
+        }
+    }
+}
