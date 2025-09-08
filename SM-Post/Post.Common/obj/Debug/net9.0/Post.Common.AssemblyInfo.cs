ea6bec96d1c5ec1414c07a518e9e9eaf27fe93b0
Gabriel Habermann <gabrielfranh@gmail.com>
2025-09-08 16:23:34 -0300

    Add hosted service and consumer service

--- SM-Post/Post.Common/obj/Debug/net9.0/Post.Common.AssemblyInfo.cs
+++ SM-Post/Post.Common/obj/Debug/net9.0/Post.Common.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Post.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c54dd6015eb244ffcd387d5602ab27057ff94265")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dea93fd072abcf8f760d9c9abd39308c1055ca6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Post.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Post.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ SM-Post/Post.Query/Post.Query.Infrastructure/Consumers/ConsumerHostedService.cs
@@ -0,0 +1,33 @@
+using CQRS.Core.Consumers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Post.Query.Infrastructure.Consumers
+{
+    public class ConsumerHostedService(ILogger<ConsumerHostedService> logger, IServiceProvider serviceProvider) : IHostedService
+    {
+        private readonly ILogger<ConsumerHostedService> _logger = logger;
+        private readonly IServiceProvider _serviceProvider = serviceProvider;
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Event consumer service running");
+
+            using (IServiceScope scope = _serviceProvider.CreateScope())
+            {
+                var eventConsumer = scope.ServiceProvider.GetRequiredService<IEventConsumer>();
+                var topic = Environment.GetEnvironmentVariable("KAFKA_TOPIC");
+                Task.Run(() => eventConsumer.Consume(topic), cancellationToken);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Event consumer service stopped");
+
+            return Task.CompletedTask;
+        }
+    }
+}
