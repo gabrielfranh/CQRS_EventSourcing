47da91e80812f329e859b187398ee759cb73dd47
Gabriel Habermann <gabrielfranh@gmail.com>
2025-09-08 14:02:49 -0300

    Add domain layer entities

--- SM-Post/Post.Query/Post.Query.Domain/obj/Debug/net9.0/Post.Query.Domain.AssemblyInfo.cs
+++ SM-Post/Post.Query/Post.Query.Domain/obj/Debug/net9.0/Post.Query.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Post.Query.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26fa954f385a69c10546814d2bcd2cd4c37b579a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbc8d06cfaae8eebb423c420a7264252390732f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Post.Query.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Post.Query.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
