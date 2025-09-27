4b4a8b3f5e078b311ba5bf809da6764b005d02c0
Gabriel Habermann <gabrielfranh@gmail.com>
2025-09-27 12:10:44 -0300

    Adicionando queries dispatcher interface and concrete class

--- CQRS-ES/CQRS.Core/obj/Debug/net9.0/CQRS.Core.AssemblyInfo.cs
+++ CQRS-ES/CQRS.Core/obj/Debug/net9.0/CQRS.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CQRS.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38391eb42eb00e55a2db8e91f7c53d6853c2d2d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0b6e754e89916fa3856b8aaddabfab7886a1e18")]
 [assembly: System.Reflection.AssemblyProductAttribute("CQRS.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CQRS.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
